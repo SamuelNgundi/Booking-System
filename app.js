7900882d8e0fc57e12b388949e8c642717799b54
SamuelNgundi <samuelwngundi02@gmail.com>
2023-08-21 23:52:16 +0300

    Install node-express

---
+++ app.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!..');
+})
+
+app.listen(3000)
