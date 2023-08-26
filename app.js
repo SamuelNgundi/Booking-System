004c5ca9e4b8775e8bc5cbe997a62e7989e71ac4
SamuelNgundi <samuelwngundi02@gmail.com>
2023-08-26 22:34:24 +0300

    Link user with event.. ish

--- app.js
+++ app.js
@@ -79,13 +79,25 @@ app.use('/graphql', graphqlHTTP({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+                date: new Date(args.eventInput.date),
+                creator: '64ea4a01b4ae24472a4d0c50'
             });
+            let createdEvent;
             return event
            .save()
            .then(result => {
-            console.log(result);
-            return {...result._doc};
+            createdEvent = {...result._doc};
+            return User.findById('64ea4a01b4ae24472a4d0c50')
+           })
+           .then(user => {
+            if (!user) {
+                throw new Error('User not found.');
+            }
+            user.createdEvents.push(event);
+            return user.save();
+           })
+           .then (result => {
+            return createdEvent;
            })
            .catch(err => {
             console.log(err);
