2f4092d73e94102b4eb9f17eac0f5ffaff82455c
SamuelNgundi <samuelwngundi02@gmail.com>
2023-09-01 18:27:35 +0300

    change to async-await

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,29 +4,51 @@ const Event = require('../../models/event');
 const User = require('../../models/user');
 
 
-const events = eventIds => {
-    return Event.find({_id: {$in: eventIds}})
-    .then(events => {
-        return events.map(event =>{
+// const events = eventIds => {
+//     return Event.find({_id: {$in: eventIds}})
+//     .then(events => {
+//         return events.map(event =>{
+//             return { ...event._doc,
+//                 date: new Date(event._doc.date).toISOString(),
+//                 creator: user.bind(this, event.creator) }
+//         })
+//     }) 
+//     .catch( err =>{
+//         throw err
+//     })
+// }
+const events = async eventIds => {
+    try {
+        const events = await Event.find({_id: {$in: eventIds}});
+        events.map(event => {
             return { ...event._doc,
-                date: new Date(event._doc.date).toISOString(),
-                creator: user.bind(this, event.creator) }
-        })
-    }) 
-    .catch( err =>{
-        throw err
-    })
-}
-
-const user = userId => {
-    return User.findById(userId)
-    .then(user => {
-        return { ...user._doc, createdEvents: events.bind(this, user._doc.createdEvents) };
-    })
-    .catch( err => {
+                    date: new Date(event._doc.date).toISOString(),
+                    creator: user.bind(this, event.creator) };
+        });
+        return events;
+    } catch (err) {
         throw err;
-    })
-}
+    }
+};
+
+// const user = userId => {
+//     return User.findById(userId)
+//     .then(user => {
+//         return { ...user._doc, createdEvents: events.bind(this, user._doc.createdEvents) };
+//     })
+//     .catch( err => {
+//         throw err;
+//     })
+// }
+
+const user = async userId => {
+    try {
+        const user = await User.findById(userId);
+        return { ...user._doc, createdEvents: events.bind(this, user._doc.createdEvents) };
+    } catch (err) {
+        throw err;
+    }
+};
 
 module.exports = {
     events: () => {
