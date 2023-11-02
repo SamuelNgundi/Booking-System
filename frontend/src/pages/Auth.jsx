ff5e5e831b9ab83780142029a3aab7684aa9d0e7
SamuelNgundi <samuelwngundi02@gmail.com>
2023-11-02 21:00:33 +0300

    pills and potion

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -86,7 +86,7 @@ function Auth() {
         <MDBTabsPane show={justifyActive === 'tab2'}>
 
           <div className="text-center mb-3">
-            <p>Sign un with:</p>
+            <p>Sign up with:</p>
 
             <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
               <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
