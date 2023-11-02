3b91fa74fe0ee5b0b0a9fef602a523791d5babfb
SamuelNgundi <samuelwngundi02@gmail.com>
2023-11-02 20:12:19 +0300

    33

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,18 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Auth from './pages/Auth';
 import Bookings from './pages/Bookings';
 import Events from './pages/Events';
-import Navbar from './components/Navigation/NavBar';
+import './App.css'
 
 const App = () => {
   return (
+    <>
     <Routes>
       <Route path="/" element={ <Navigate to="/auth" /> }/>
       <Route path="/auth" element={ <Auth /> }/>
       <Route path="/bookings" element={ <Bookings /> }/>
       <Route path="/events" element={ <Events /> }/>
-      <Route path="/navbar" element={ <Navbar /> }/>
     </Routes>
+    </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import NavBar from './components/Navigation/NavBar.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-    <App />
+    <NavBar/>
+    <div className='main-content'>
+      <App />
+    </div>
     </Router>
   </React.StrictMode>,
 )
