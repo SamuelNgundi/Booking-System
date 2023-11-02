3b5f85772b3b172ca60d9162562294090dd0d694
SamuelNgundi <samuelwngundi02@gmail.com>
2023-11-02 18:33:25 +0300

    Update

---
+++ frontend/src/components/Navigation/NavBar.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+        <LinkContainer to="/"><Navbar.Brand>EventBooking</Navbar.Brand> </LinkContainer>
+          <Nav className="me-auto">
+          <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+            <LinkContainer to="/bookings"><Nav.Link>Bookings</Nav.Link></LinkContainer>
+            <LinkContainer to="/events"><Nav.Link>Events</Nav.Link></LinkContainer>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
