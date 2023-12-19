3f02db620250b19f62d6c7632e2a991676bbf799
SamuelNgundi <samuelwngundi02@gmail.com>
2023-12-19 15:20:36 +0300

    context..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,18 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Auth from './pages/Auth';
 import Bookings from './pages/Bookings';
 import Events from './pages/Events';
+import authContext from './context/auth-context';
 
 const App = () => {
   return (
     <>
     <Routes>
+      <authContext.Provider>
       <Route path="/" element={ <Navigate to="/auth" /> }/>
       <Route path="/auth" element={ <Auth /> }/>
       <Route path="/bookings" element={ <Bookings /> }/>
       <Route path="/events" element={ <Events /> }/>
+      </authContext.Provider>
     </Routes>
     </>
   )
